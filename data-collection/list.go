16448498522f9b5be66f1fddf7111f3b9635e651
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-07-14 23:40:32 +0000

    bug fixes

--- data-collection/list.go
+++ data-collection/list.go
@@ -2,14 +2,33 @@ package main
 
 import "fmt"
 
-func main () {
-	var productNames [4]string; 
-	prices := [4]float32{45.665,43.54,343.45,56.34}
-	productNames[3] = "Books"
-	featuredPrice := prices[1:]
-	highlightedPrices := featuredPrice[:1]
-	fmt.Println(prices[3])
-	fmt.Println(productNames[3])
-	fmt.Println(featuredPrice)
-	fmt.Print(len(highlightedPrices), cap(featuredPrice))
+
+
+func main() {
+	prices := []float64{45.665,43.54,343.45,56.34}
+	fmt.Println((prices[0:1]))
+
+	prices[1] = 99.9
+
+
+	prices = append(prices, 5.66,234.34,344.34)
+
+	prices = prices[1:]
+
+	fmt.Println(prices)
+
+
 }
+
+
+// func main () {
+// 	var productNames [4]string; 
+// 	prices := [4]float32{45.665,43.54,343.45,56.34}
+// 	productNames[3] = "Books"
+// 	featuredPrice := prices[1:]
+// 	highlightedPrices := featuredPrice[:1]
+// 	fmt.Println(prices[3])
+// 	fmt.Println(productNames[3])
+// 	fmt.Println(featuredPrice)
+// 	fmt.Print(len(highlightedPrices), cap(featuredPrice))
+// }
