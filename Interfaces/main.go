cac6666f93a9c9c4852c60eb3c276866451b8127
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-07-22 23:42:00 +0000

    interfaces

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -6,6 +6,10 @@ import (
 	"io/ioutil"
 )
 
+type logger interface {
+	log()
+}
+
 type logData struct {
 	message  string
 	fileName string
@@ -16,6 +20,8 @@ func (lData *logData) log() {
 
 	if err != nil {
 		fmt.Println("Storing the log data failed!")
+		message := err.Error()
+		fmt.Println(message)
 	}
 }
 
@@ -26,11 +32,54 @@ func (text loggableString) log() {
 }
 
 func main() {
-	userLog := logData{"User logged in", "user-log.txt"}
+	userLog := &logData{"User logged in", "user-log.txt"}
 	//do more work ...
-	userLog.log()
+	// userLog.log()
+	createLog(userLog)
 
 	message := loggableString("[INFO] User created!")
 	//do more work...
-	message.log()
+	// message.log()
+	createLog(message)
+	outputValue(message)
+
+	firstNumber := 5
+	secondNumber := 10.1
+	numbers:= []int{1,2,4,5}
+
+	doubledFirstNumber := double(firstNumber)
+	doubledSecondNumber := double(secondNumber)
+	doubledNumbers := double(numbers)
+	
+	fmt.Println(doubledFirstNumber)
+	fmt.Println(doubledSecondNumber)
+	fmt.Println(doubledNumbers)
+}
+
+func createLog(data logger) {
+	//more things we do
+	data.log()
+}
+
+func outputValue (value interface{}) {
+	val, ok := value.(loggableString)
+	fmt.Println(val, ok)
+	fmt.Println(value)
+}
+
+func double (value interface{}) interface{}{
+	switch val := value.(type) {
+	case int:
+		return val * 2
+
+	case float64:
+		return val * 2 
+	case []int:
+		newNumbers:= append(val, val...)
+		return newNumbers
+	default:
+		return ""
+	}
+	
+
 }
