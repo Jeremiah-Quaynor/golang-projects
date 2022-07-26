e6082e1bb116e53122b724d4620b0140090937bb
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-07-26 00:52:17 +0000

    bug fix

--- goroutines/main.go
+++ goroutines/main.go
@@ -11,18 +11,49 @@ var source = rand.NewSource(time.Now().Unix())
 var randN = rand.New(source)
 
 func main() {
+	x := make(chan int)
+	y := make(chan int)
+	limiter := make(chan int, 3)
 
-	x := generateValue()
-	y := generateValue()
 
-	sum := x +y 
+	go generateValue(x, limiter)
+	go generateValue(y, limiter)
 
-	fmt.Print(sum)
+
+	var a int 
+	var b int
+
+	select {
+	case a = <- x:
+		fmt.Printf("X finished first. value is: %v",a)
+	case b = <-y:
+		fmt.Printf("Y finished first. value is: %v",b)
+
+	}
+
+
+	// go generateValue(c, limiter)
+	// go generateValue(c, limiter)
+	// sum := 0
+	// i := 0
+	// for num := range c {
+	// 	sum += num
+	// 	i ++
+	// 	if i ==4 {
+	// 	close(c)
+	// 	}
+	// }
+	// fmt.Print(sum)
 }
 
-func generateValue() int{
+func generateValue(c chan int, limit chan int) int{
+	limit <- 1
+	fmt.Println("Generating value...")
 	sleepTime := rand.Intn(3)
 	time.Sleep(time.Duration(sleepTime) * time.Second)
+	result := randN.Intn(10)
 
-	return randN.Intn(10)
+	c <- result
+	<- limit
+	return result
 }
