c242b8616b49f1d52ed033e3ccd3de391107cf78
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-07-28 23:55:49 +0000

    ATM phase one

--- ATM-Simulator/main.go
+++ ATM-Simulator/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
-var reader = bufio.NewReader(os.Stdin)
+// var reader = bufio.NewReader(os.Stdin)
 
 func main () {
+	welcomeMessage()
+	getDetails()
+	verifyDetails()
+	performTransaction()
+	terminateSession()
+
+}
+
+func welcomeMessage() {
 	fmt.Println("############################")
 	fmt.Println("Welcome To Your Friendly ATM")
+	fmt.Println("-------------------")
+}
 
-	
+func getDetails () (int, int) {
+	var bankAcc, pin int 
 
+	fmt.Print("Please Your Account Number: ")
+	fmt.Scan(&bankAcc)
+	fmt.Print("Please Your Account Pin: ")
+	fmt.Scan(&pin)
+
+	if bankAcc == 0 || pin == 0 {
+	fmt.Print("Account number or Pin invalid")
+	return 0,0
+	}
+	return bankAcc,pin
+
+}
+
+func verifyDetails () {
+
+}
+
+func performTransaction () {
+
+}
+
+
+func terminateSession () {
 
 }
